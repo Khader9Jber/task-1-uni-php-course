6daf891d0156d68dbaca5ce454ad93e528ee09a9
KhaderKaraja <khderjber@gmail.com>
2022-11-03 09:52:21 +0200

    HW 1 - Connect to the DB using PDO and test the connection

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$dsn = 'mysql:host=localhost;port=3307;dbname=test_connection';
+$user = 'root';
+$pass = '';
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+
+);
+try {
+    $db_connection = new PDO($dsn, $user, $pass, $options);
+    $sql = "INSERT INTO `students`(`first_name`, `last_name`, `gpa`, `level`) VALUES ('Khader','Karaja','95','4')";
+    $db_connection->prepare($sql)->execute();
+    echo 'Connect & Insert Successfully ^_^';
+} catch (PDOException $e) {
+    echo 'Failed to connect => ' . $e->getCode() . ' : ' . $e->getMessage();
+}
